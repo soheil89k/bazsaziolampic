c8827445b923ed474d392492b5cf3afbb4e8fc19
soheil89k <35937792+soheil89k@users.noreply.github.com>
2022-12-12 14:29:49 +0330

    a

--- application/logs/log-2022-12-11.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-12-11 13:04:32 --> Severity: Notice --> Trying to get property 'version' of non-object /home/bazsaziolampic/crm/application/libraries/App.php 110
-ERROR - 2022-12-11 13:04:32 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/bazsaziolampic/crm/system/core/Exceptions.php:271) /home/bazsaziolampic/crm/system/core/Common.php 570
